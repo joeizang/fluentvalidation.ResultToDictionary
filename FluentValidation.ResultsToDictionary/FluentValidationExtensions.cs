4b3d52a59f0a914bc4a8d78c6dc64223614b4b48
Joseph Izang <josephizang@gmail.com>
2022-07-10 13:55:12 +0100

    add code for project

---
+++ FluentValidation.ResultsToDictionary/FluentValidationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+
+namespace FluentValidation.ResultsToDictionary
+{
+    public static class FluentValidationExtensions
+    {
+        public static IDictionary<string, string[]> ToDictionary(this ValidationResult result)
+        {
+            var conversion = result.Errors
+                .GroupBy(x => x.PropertyName)
+                .ToDictionary(y => 
+                        y.Key, 
+                    y => 
+                        y.Select(x => x.ErrorMessage).ToArray());
+            return conversion;
+        }
+    }   
+}
